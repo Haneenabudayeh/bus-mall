581ba0f9f616f9d3ca54be5eca3276adf2337b66
Haneenabudayeh <60567032+Haneenabudayeh@users.noreply.github.com>
2020-03-03 10:56:53 +0200

    change js

--- app.js
+++ app.js
@@ -2,133 +2,83 @@
 
 //Array of products
 Product.allProducts = [];
-Product.activeSet = [];
-Product.lastDisplayed = [];
-Product.totalVotes = 25;
 
-//Arrays to hold data for the chart
-var votes = [];
-var names = [];
-//Products Section
-Product.section = document.getElementById('productsSection');
-//Results Element
-Product.resultsList = document.getElementById('resultsList');
-//Referring to specific images
+//Variables in use
 var productOne = document.getElementById('productOne');
 var productTwo = document.getElementById('productTwo');
 var productThree = document.getElementById('productThree');
+var activeSet = [];
+// var excludeSet = [];
+var votes = 25;
 
-function Product(name, filepath, altText) {
+function Product(name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  this.altText = altText;
-  this.votes = 0;
+  this.clicks = 0;
   this.views = 0;
   Product.allProducts.push(this);
 }
 
 //Make new Products instances
-new Product('Luggage', 'img/bag.jpg','Bag');
-new Product('Banana Slicer', 'img/banana.jpg','Banana Slicer');
-new Product('Bathroom Buddy', 'img/bathroom.jpg','Bathroom');
-new Product('Fashion Wellies', 'img/boots.jpg','Boots');
-new Product('Breakfast Machine', 'img/breakfast.jpg','Breakfast');
-new Product('Meatball Bubblegum', 'img/bubblegum.jpg', 'Bubblegum');
-new Product('Chair', 'img/chair.jpg', 'Chair');
-new Product('Cthulhu', 'img/cthulhu.jpg', 'Cthulhu');
-new Product('Duck Muzzle', 'img/dog-duck.jpg', 'Dog Duck');
-new Product('Dragon Meat', 'img/dragon.jpg', 'Dragon Meat');
-new Product('Practical Cutlery', 'img/pen.jpg', 'Pen');
-new Product('Pet Broom', 'img/pet-sweep.jpg', 'Pet Broom');
-new Product('Pizza Scissors', 'img/scissors.jpg', 'Scissors');
-new Product('Shark Attack', 'img/shark.jpg', 'Shark');
-new Product('Baby Broom', 'img/sweep.png', 'Sweep');
-new Product('Tauntaun', 'img/tauntaun.jpg', 'Tauntaun');
-new Product('Unicorn Meat', 'img/unicorn.jpg', 'Unicorn Meat');
-new Product('Tentacle USB', 'img/usb.gif', 'Tentacle USB');
-new Product('Watering Can', 'img/water-can.jpg', 'Watering Can');
-new Product('Wine Glass', 'img/wine-glass.jpg', 'Wine Glass');
+new Product('Bag', 'img/bag.jpg');
+new Product('Banana Slicer', 'img/banana.jpg');
+new Product('Bathroom', 'img/bathroom.jpg');
+new Product('Boots', 'img/boots.jpg');
+new Product('Breakfast', 'img/breakfast.jpg');
+new Product('Bubblegum', 'img/bubblegum.jpg');
+new Product('Chair', 'img/chair.jpg');
+new Product('Cthulhu', 'img/cthulhu.jpg');
+new Product('Dog Duck', 'img/dog-duck.jpg');
+new Product('Dragon Meat', 'img/dragon.jpg');
+new Product('Pen', 'img/pen.jpg');
+new Product('Pet Broom', 'img/pet-sweep.jpg');
+new Product('Scissors', 'img/scissors.jpg');
+new Product('Shark', 'img/shark.jpg');
+new Product('Sweep', 'img/sweep.png');
+new Product('Tauntaun', 'img/tauntaun.jpg');
+new Product('Unicorn Meat', 'img/unicorn.jpg');
+new Product('Tentacle USB', 'img/usb.gif');
+new Product('Watering Can', 'img/water-can.jpg');
+new Product('Wine Glass', 'img/wine-glass.jpg');
 
-//Retrieving data from local storage, if it exists
-if (localStorage.getItem('storedProducts') !== null) {
-  console.log('Data found');
-  Product.allProducts = JSON.parse(localStorage.getItem('storedProducts'));
-} else {
-  console.log('Not found');
-  localStorage.setItem('storedProducts', JSON.stringify(Product.allProducts));
-}
+//Event Listener
 
+productOne.addEventListener('click', randomProduct);
+productTwo.addEventListener('click', randomProduct);
+productThree.addEventListener('click', randomProduct);
 
 //Randomly display products
 
+function randomSet() {
+  activeSet = [];
+  while(activeSet.length < 3){
+    var randomNumber = Math.floor(Math.random() * Product.allProducts.length);
+    // if (randomNumber == excludeSet[i])
+    if(activeSet.indexOf(randomNumber) > - 1) continue;
+    activeSet.push(randomNumber);
+  }
+}
+
 function randomProduct() {
-  var randomOne = Math.floor(Math.random() * Product.allProducts.length);
-  var randomTwo = Math.floor(Math.random() * Product.allProducts.length);
-  var randomThree = Math.floor(Math.random() * Product.allProducts.length);
+  randomSet();
+  productOne.src = Product.allProducts[activeSet[0]].filepath;
+  productTwo.src = Product.allProducts[activeSet[1]].filepath;
+  productThree.src = Product.allProducts[activeSet[2]].filepath;
 
-  //Confirm there are no duplicate images, and if there are, reroll
-  while(Product.lastDisplayed.includes(randomOne) || Product.lastDisplayed.includes(randomTwo) || Product.lastDisplayed.includes(randomThree) || randomOne === randomTwo || randomTwo == randomThree || randomThree == randomOne) {
-    randomOne = Math.floor(Math.random() * Product.allProducts.length);
-    randomTwo = Math.floor(Math.random() * Product.allProducts.length);
-    randomThree = Math.floor(Math.random() * Product.allProducts.length);
-  }
-
-  // Update images
-  productOne.src = Product.allProducts[randomOne].filepath;
-  productTwo.src = Product.allProducts[randomTwo].filepath;
-  productThree.src = Product.allProducts[randomThree].filepath;
-  productOne.altText = Product.allProducts[randomOne].altText;
-  productTwo.altText = Product.allProducts[randomTwo].altText;
-  productThree.altText = Product.allProducts[randomThree].altText;
-
-  // Increments views for all images
-  Product.allProducts[randomOne].views++;
-  Product.allProducts[randomTwo].views++;
-  Product.allProducts[randomThree].views++;
-
-  Product.lastDisplayed[0] = randomOne;
-  Product.lastDisplayed[1] = randomTwo;
-  Product.lastDisplayed[2] = randomThree;
+  Product.allProducts[activeSet[0]].views++;
+  Product.allProducts[activeSet[1]].views++;
+  Product.allProducts[activeSet[2]].views++;
+  votes--;
 }
 
-//Event Handler function
-function newSet (event) {
-
-  if (event.target.id === 'productsSection') {
-    return alert('Please click on an image.');
-
-  }
-//Decrement total available votes
-  Product.totalVotes--;
-
-//Count individual product votes
-  for(var i = 0; i < Product.allProducts.length; i++) {
-    if(event.target.altText === Product.allProducts[i].altText) {
-      Product.allProducts[i].votes++;
-      updateChartArrays();
-    }
-  }
-  if (Product.totalVotes < 1) {
-    Product.section.removeEventListener('click', newSet);
-    productsSection.innerHTML = '';
-    localStorage.setItem('storedProducts', JSON.stringify(Product.allProducts));
-    drawChart();
-  }
-  randomProduct();
+//Trying to trigger the event listeners to disable after 25 votes :( Not working. Also tried some loops. 
+if (votes == 0) {
+  productOne.removeEventListener('click', randomProduct);
+  productTwo.removeEventListener('click', randomProduct);
+  productThree.removeEventListener('click', randomProduct);
 }
 
-//Update data arrays 
-function updateChartArrays() {
-  for (var i = 0; i < Product.allProducts.length; i++) {
-    names[i] = Product.allProducts[i].name;
-    votes[i] = Product.allProducts[i].votes;
-  }
-}
-
-// 
-
-
-//Event Listener
-Product.section.addEventListener('click', newSet);
 
 randomProduct();
+console.log(activeSet);
+console.log(Product.allProducts);
